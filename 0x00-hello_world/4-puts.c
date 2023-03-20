8c469c79819a42ea790259a6801216bac4e5d882
kkebuka <kkebuka@gmail.com>
2023-03-20 07:30:15 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 *main - this is a script that uses puts not printf
 *return: 0 (success)
 */
-void main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
