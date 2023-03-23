79bb68d5bc315be5942a7bcf7ab778ff9bbd13cf
kkebuka <kkebuka@gmail.com>
2023-03-22 18:13:39 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -  I sometimes suffer from insomnia
+ * And when I can't fall asleep, I play what I call the alphabet game
+ * Return: 0 success
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
