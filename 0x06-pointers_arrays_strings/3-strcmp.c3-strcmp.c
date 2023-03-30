89dd3c7b67047e7a88098ca8fd18a1b21aee8e09
kkebuka <kkebuka@gmail.com>
2023-03-30 11:34:47 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c3-strcmp.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp - function that compares two strings
+ * @s1: input value
+ * @s2: input value
+ * Return: s2
+ */
+int _strcmp(char *s1, char *s2)
+{
